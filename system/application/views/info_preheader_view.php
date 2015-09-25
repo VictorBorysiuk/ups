2105ce2c2bae0aa4701829a7b044f876b1549a35
Vitia_ <borysyukviktor@gmail.com>
2015-09-25 14:29:33 +0300

    Загрузка шаблону з якого буде починатися переробка

---
+++ system/application/views/info_preheader_view.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=windows-1251">
+    <title>УФТ :: Українське фізичне товариство </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <!-- Bootstrap -->
+	<link href="<?=base_url();?>styles/style.css" rel="stylesheet">
+    <link href="<?=base_url();?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=base_url();?>css/custom.css" rel="stylesheet">
+    <script src="<?=base_url();?>js/respond.js"></script>
+</head>
+
+<body>
+<a name="top"></a>
+<div class="container">
