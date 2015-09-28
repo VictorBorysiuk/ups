f64465759f23eedc6e3dfec721168ab1dfbd6098
Vitia_ <borysyukviktor@gmail.com>
2015-09-28 15:44:03 +0300

    добавив меню

--- www/system/application/views/top_navigation_view.php
+++ www/system/application/views/top_navigation_view.php
@@ -13,3 +13,32 @@
 </tr>
 </table> 
 </div>
+
+<div class="row">
+    	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="glyphicon glyphicon-arrow-down"></span>
+                  MENU
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="collapse">
+                <ul class="nav navbar-nav">
+                    <li ><a href="<?=base_url();?>index_u.html">Головна</a></li>
+                    <li class="dropdown active"><a href="#" data-toggle="dropdown">З'їзди УФТ <span class="caret"></span></a>
+                    	<ul class="dropdown-menu">
+                            <li><a href="<?=base_url();?>VII.html">VII</a></li>
+                            <li><a href="<?=base_url();?>VIII.html">VIII</a></li>
+                        </ul>                    
+                    </li>
+                    <li ><a href="<?=base_url();?>statut.html">Нормативні документи</a></li>
+                    <li><a href="<?=base_url();?>member.html">Членство в УФТ</a></li>  
+                    <li><a href="<?=base_url();?>press.html">Публікації в пресі</a></li>
+                    <li><a href="<?=base_url();?>confer.html">Конференції</a></li>
+                    <li><a href="<?=base_url();?>news.html">Новини товариства</a></li>
+                    <li><a href="<?=base_url();?>bureau.html">Засідання бюро</a></li>
+                </ul>
+            </div>
+         </nav> 
+    </div>
