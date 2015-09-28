41ce3033c0b2656ddc799e4c83cee8fe6aca656f
Vitia_ <borysyukviktor@gmail.com>
2015-09-28 15:34:59 +0300

    перенесені файли

--- system/application/views/header_view.php
+++
@@ -1,7 +0,0 @@
-<div id="header"><img src="<?=base_url();?>img/header.jpg" alt = "" usemap="#standard">
-
-<map name="standard">
-<area shape = "rect" href = "<?=base_url();?>" coords = "50,20,500,100" alt="">
-</map>
-
-</div>
--- system/application/views/info_view.php
+++
@@ -1,7 +0,0 @@
-<div id="wrapper">
-    <div id="content">
-
-<p><strong><?=$info;?></strong></p>
-
-    </div>
-</div>
